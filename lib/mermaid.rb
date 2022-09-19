1510fd1c9d5fcc45c7bb7aee8a4ae7ef090093ec
Alastair Lloyd <dex8056@aol.com>
2022-09-19 08:13:01 -0600

    Initial commit

--- lib/mermaid.rb
+++ lib/mermaid.rb
@@ -1,13 +1,13 @@
 # mermaid_spec.rb
 class Mermaid
-    
+
 attr_reader :name
 attr_reader :age
-attr_reader :older
-    
+attr_reader :get_older
+
     def initialize(name)
     @name = name
-    @age = age
-    @older = older
+    @age = 22
+    @get_older = false
   end
 end
